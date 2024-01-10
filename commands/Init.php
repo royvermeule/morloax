e59410349b1a2b1cc05c2dd6e8b4a4dad606ad01
Roy Vermeulen <rvermeulen@eljakim.nl>
2024-01-10 01:24:15 +0100

    init script fixed

--- commands/Init.php
+++ commands/Init.php
@@ -9,8 +9,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Init extends Command
 {
     private const commandName = 'morloax:init';
-    private string $rootDir = __DIR__ . '/../../../../';
-    private string $initDir = __DIR__ . '/utility/shells/init/';
 
     public function configure()
     {
@@ -26,69 +24,69 @@ class Init extends Command
         $this->initMiddleware();
         $this->initRoutes();
 
+        if (file_exists(__DIR__ . '/utility/shells/init/htaccess.txt')) {
+            $output->writeln('hello');
+        }
+
+
         return Command::SUCCESS;
     }
 
     public function initRoot(): void
     {
-        $htacces = file_get_contents($this->initDir . 'htaccess.txt');
-        file_put_contents($this->initDir . '.htaccess', $htacces);
-
-        $migrations = file_get_contents($this->initDir . 'migrations.txt');
-        file_put_contents($this->rootDir . 'migrations.php', $migrations);
-
-        $migrationsDb = file_get_contents($this->initDir . 'migrations-db.txt');
-        file_put_contents($this->rootDir . 'migrations-db.php', $migrationsDb);
+        $this->createFile('htaccess.txt', '.htaccess');
+        $this->createFile('migrations.txt', 'migrations.php');
+        $this->createFile('migrations-db.txt', 'migrations-db.php');
     }
 
     public function initPublic(): void
     {
-        $htaccess = file_get_contents($this->initDir . 'public/htaccess.txt');
-        file_put_contents($this->rootDir . 'public/.htaccess', $htaccess);
-
-        $frontController = file_get_contents($this->initDir . 'public/index.txt');
-        file_put_contents($this->rootDir . 'public/index.php', $frontController);
+        $this->createDirectory(__DIR__ . '/../../../../public');
+        $this->createFile('public/htaccess.txt', 'public/.htaccess');
+        $this->createFile('public/index.txt', 'public/index.php');
     }
 
     public function initApp(): void
     {
-        $htaccess = file_get_contents($this->initDir . 'app/htaccess.txt');
-        file_put_contents($this->rootDir . 'app/.htaccess', $htaccess);
-
-        $controller = file_get_contents($this->initDir . 'app/HomeController.txt');
-        file_put_contents($this->rootDir . 'app/Controller/HomeController.php', $controller);
-
-        $homeView = file_get_contents($this->initDir . 'app/View/index.txt');
-        file_put_contents($this->rootDir . 'app/View/index.php', $homeView);
-
-        $headSection = file_get_contents($this->initDir . 'app/View/sections/head.txt');
-        file_put_contents($this->rootDir . 'app/View/sections/head.php', $homeView);
-
-        if (!mkdir($concurrentDirectory = $this->rootDir . 'app/Model') && !is_dir($concurrentDirectory)) {
-            throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
-        }
+        $this->createDirectory(__DIR__ . '/../../../../app');
+        $this->createDirectory(__DIR__ . '/../../../../app/Controller');
+        $this->createDirectory(__DIR__ . '/../../../../app/View');
+        $this->createDirectory(__DIR__ . '/../../../../app/View/sections');
+        $this->createDirectory(__DIR__ . '/../../../../app/Model');
+        $this->createFile('app/Controller/HomeController.txt', 'app/Controller/HomeController.php');
+        $this->createFile('app/View/index.txt', 'app/View/index.php');
+        $this->createFile('app/View/sections/head.txt', 'app/View/sections/head.php');
+        $this->createFile('app/htaccess.txt', 'app/.htaccess');
     }
 
     public function initMiddleware(): void
     {
-        $middlewareDir = $this->initDir . '/middleware';
-
-        $middleware = file_get_contents($middlewareDir . '/Middleware.txt');
-        file_put_contents($this->rootDir . 'middleware/Middleware.php', $middleware);
-
-        $csrf = file_get_contents($middlewareDir . '/Csrf.txt');
-        file_put_contents($this->rootDir . 'middleware/Csrf.php', $csrf);
-
-        $session = file_get_contents($middlewareDir . '/Session.txt');
-        file_put_contents($this->rootDir . 'middleware/Session.php', $session);
-
-        $routeExists = file_get_contents($middlewareDir . '/RouteExists.txt');
-        file_put_contents($this->rootDir . 'middleware/RouteExists.php', $routeExists);
+        $this->createDirectory(__DIR__ . '/../../../../middleware');
+        $middlewareDir = 'middleware';
+        $this->createFile($middlewareDir . '/Middleware.txt', 'middleware/Middleware.php');
+        $this->createFile($middlewareDir . '/Csrf.txt', 'middleware/Csrf.php');
+        $this->createFile($middlewareDir . '/Session.txt', 'middleware/Session.php');
+        $this->createFile($middlewareDir . '/RouteExists.txt', 'middleware/RouteExists.php');
     }
 
     public function initRoutes(): void
     {
-        $web = file_get_contents($this->initDir . 'routes/web.txt');
-        file_put_contents($this->rootDir . 'routes/web.php', $web);
+        $this->createDirectory(__DIR__ . '/../../../../routes');
+        $this->createFile('routes/web.txt', 'routes/web.php');
+    }
+
+    private function createDirectory(string $directory): void
+    {
+        if (!file_exists($directory)) {
+            if (!mkdir($directory, 0777, true) && !is_dir($directory)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $directory));
+            }
+        }
+    }
+
+    private function createFile(string $source, string $destination): void
+    {
+        $content = file_get_contents(__DIR__ . '/utility/shells/init/' . $source);
+        file_put_contents(__DIR__ . '/../../../../' . $destination, $content);
     }
 }
