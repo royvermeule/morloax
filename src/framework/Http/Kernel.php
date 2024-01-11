e96a8173b9767b018236f0dbbe6519b3f749398c
Roy Vermeulen <rvermeulen@eljakim.nl>
2024-01-11 10:50:36 +0100

    added ability to add middleware to specific routes

--- src/framework/Http/Kernel.php
+++ src/framework/Http/Kernel.php
@@ -10,27 +10,40 @@ use function FastRoute\simpleDispatcher;
 class Kernel
 {
     private array $middleware = [];
+    private array $routes;
 
-    public function addMiddleware(callable $middleware): void
+    public function __construct()
     {
-        $this->middleware[] = $middleware;
+        $routes = include BASE_PATH . '/routes/web.php';
+        $this->routes = $routes;
+
+        $this->addMiddleware();
+    }
+
+    public function addMiddleware(): void
+    {
+        foreach ($this->routes as $route) {
+            if (isset($route[3])) {
+                $url = $route[1];
+                $middleware = $route[3];
+
+                $this->middleware[$url] = $middleware;
+            }
+        }
+    }
+
+    public function addMiddlewareToAll(array $middleware): void
+    {
+        $this->middleware['all'] = $middleware;
     }
 
     public function handle(Request $request): Response
     {
-        foreach ($this->middleware as $middleware) {
-            $response = call_user_func($middleware, $request);
-
-            if ($response instanceof Response) {
-                return $response;
-            }
-        }
+        $this->executeMiddleware();
 
         $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
-            $routes = include BASE_PATH . '/routes/web.php';
-
-            foreach ($routes as $route) {
-                $routeCollector->addRoute(...$route);
+            foreach ($this->routes as $route) {
+                $routeCollector->addRoute($route[0], $route[1], $route[2]);
             }
         });
 
@@ -42,5 +55,47 @@ class Kernel
         [$status, [$controller, $method], $vars] = $routeInfo;
 
         return call_user_func_array([new $controller, $method], $vars);
+    }
+
+    public function hasMiddleware(string $uri): bool
+    {
+        if (isset($this->middleware[$uri])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function executeMiddleware(): ?Response
+    {
+        $request = Request::createFromGlobals();
+
+        if ($this->middleware['all']) {
+            $middlewares = $this->middleware['all'];
+
+            foreach ($middlewares as $middleware => $item) {
+                $response = call_user_func(new $item, $request);
+
+                if ($response instanceof Response) {
+                    return $response;
+                }
+            }
+        }
+
+        $pathInfo = $request->getPathInfo();
+
+        if ($this->hasMiddleware($pathInfo)) {
+            $middlewares = $this->middleware[$pathInfo];
+
+            foreach ($middlewares as $middleware => $item) {
+                $response = call_user_func(new $item, $request);
+
+                if ($response instanceof Response) {
+                    return $response;
+                }
+            }
+        }
+
+        return null;
     }
 }
